dcb94c07a1b9db2dd76df6b55beda5ba62001356
bloodstalker <thabogre@gmail.com>
2017-12-20 20:15:32 +0330

    WIP

---
+++ SHA3.h
@@ -0,0 +1,93 @@
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#define bit_t unsigned char
+bit_t A[][][];
+bit_t Ap[][][];
+bit_t B[][][];
+bit_t C[][];
+bit_t D[][];
+bit_t E[][][];
+
+int b;
+int w;
+
+unsigned int16_t b_allowed[] = {25, 50, 100, 200, 400, 800, 1600};
+unsigned int8_t w_allowed[] = {1, 2 , 4, 8, 16, 32, 64}; // w = b/25
+unsigned int8_t l_allowed[] = {0, 1, 2, 3, 4, 5, 6}; // l = log(b)
+
+void theta(void) {
+  for (int x = 0; x < 5; ++x) {
+    for (int z = 0; z < w; ++z) {
+      C[x][z] = A[x][0][z] ^ A[x][1][z] ^ A[x][2][z] ^ A[x][3][z] ^ A[x][4][z];
+      D[x][z] = C[(x-1) % 5][z] ^ C[(x+1) % 5][(z-1) % w];
+    }
+  }
+
+  for (int x = 0; x < 5; ++x) {
+    for (int y = 0; y < 5; ++y) {
+      for (int z = 0; z < w; ++z) {
+        Ap[x][y][z] = A[x][y][z] ^ D[x][z];
+      }
+    }
+  }
+}
+
+
+void rho(void) {
+  for (int z = 0; z < w; ++z) {
+    Ap[0]0[][z] = A[0][0][z];
+  }
+
+
+}
+
+void pi(void) {
+  for (int x = 0; x < 5; ++x) {
+    for (int y = 0; y < 5; ++y) {
+      for (int z = 0; z < w; ++z) {
+        Ap[x][y][z] = A[(x + 3*y) % 5][x][z];
+      }
+    }
+  }
+}
+
+void chi(void) {
+  for (int x = 0; x < 5; ++x) {
+    for (int y = 0; y < 5; ++y) {
+      for (int z = 0; z < w; ++z) {
+        Ap[x][y][z] = A[x][y][z] ^ ((A[(x+1)%5][y][z] ^ 1) & A[(x + 2) % 5][y][z]);
+      }
+    }
+  }
+}
+
+void rc(void) {
+
+}
+
+void iota(void) {}
+
+void Keccak_p(void) {};
+
+/**
+ * @brief Keccak permutations
+ *
+ * @param w_ width
+ * @param r_ number of rounds to run the permutations
+ */
+void Keccak_Permutations(unsigned int16_t w_, unsigned int16_t r_) {}
+
+string SHA3_224(void) {}
+
+string SHA3_256(void) {}
+
+string SHA3_384(void) {}
+
+string SHA3_512(void) {}
+
+string SHAKE128(void) {}
+
+string SHAKE256(void) {}
+
---
+++ SHA3.hpp
@@ -0,0 +1,4 @@
+
+extern C {
+#include "./SHA3.h"
+}
